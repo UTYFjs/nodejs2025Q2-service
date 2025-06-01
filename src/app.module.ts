d49af68462494598a69350fb955770e9eff56b9b
Henadzi Suhakou <gennadiy.sugakov@gmail.com>
2025-06-01 07:17:40 +0300

    feat:add basic structure project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumsModule } from './albums/albums.module';
+import { ArtistsModule } from './artists/artists.module';
+import { FavsModule } from './favs/favs.module';
+import { TracksModule } from './tracks/tracks.module';
+import { UsersModule } from './users/users.module';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AlbumsModule,
+    ArtistsModule,
+    FavsModule,
+    TracksModule,
+    UsersModule,
+    DbModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
